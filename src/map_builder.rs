7a753b3eac7ed24e04de6db802971631b1b0a1e9
Ryan Gately <ryangately@protonmail.com>
2021-08-17 20:10:50 -0500

    added graphical layer

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -9,6 +9,18 @@ pub struct MapBuilder {
 }
 
 impl MapBuilder {
+    pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+        let mut mb = MapBuilder{
+            map: Map::new(),
+            rooms: Vec::new(),
+            player_start: Point::zero(),
+        };
+        mb.fill(TileType::Wall);
+        mb.build_random_rooms(rng);
+        mb.build_corridors(rng);
+        mb.player_start = mb.rooms[0].center();
+        mb
+    }
 
     // fill all tiles with the given type
     pub fn fill(&mut self, tile: TileType) {
@@ -51,5 +63,43 @@ impl MapBuilder {
             }
         }
         false
+    }
+
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        use std::cmp::{min, max};
+        for val in min(y1, y2) ..= max(y1, y2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, val)) {
+                self.map.tiles[idx] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        use std::cmp::{min, max};
+        for val in min(x1, x2) ..= max(x1, x2) {
+            if let Some(idx) = self.map.try_idx(Point::new(val, y)) {
+                self.map.tiles[idx] = TileType::Floor;
+            }
+        }
+    }
+
+    fn build_corridors(&mut self, rng: &mut RandomNumberGenerator) {
+        let mut rooms = self.rooms.clone();
+
+        // order rooms by center x value
+        rooms.sort_by(|a,b| a.center().x.cmp(&b.center().x));
+
+        for (i, room) in rooms.iter().enumerate().skip(1) {
+            let prev = rooms[i - 1].center();
+            let new = room.center();
+
+            if rng.range(0,2) == 1 {
+                self.apply_horizontal_tunnel(prev.x, new.x, prev.y);
+                self.apply_vertical_tunnel(prev.y, new.y, new.x);
+            } else {
+                self.apply_vertical_tunnel(prev.y, new.y, prev.x);
+                self.apply_horizontal_tunnel(prev.x, new.x, new.y);
+            }
+        }
     }
 }
